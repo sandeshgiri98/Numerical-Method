64d22f2e3837b374b2abdda607e150a15bb60db8
sandeshgiri98 <132637865+sandeshgiri98@users.noreply.github.com>
2024-01-12 08:41:15 +0545

    Matrix Inverse (Gauss Jordan)

---
+++ Matrix Inverse Using Gauss Jordan.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define SIZE 10
+int main()
+{
+float a[SIZE][SIZE], ratio;
+int i, j, k, n;
+printf("Enter size of matrix: ");
+scanf("%d", &n);
+printf("Enter Matrix coefficients:\n");
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= n; j++)
+{
+printf("a[%d][%d] = ", i, j);
+scanf("%f", &a[i][j]);
+}
+}
+for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= n; j++)
+{
+if (i == j)
+{
+a[i][j + n] = 1;
+}
+else
+{
+a[i][j + n] = 0;
+}
+}
+}
+for (i = 1; i <= n; i++)
+{
+if (a[i][i] == 0.0)
+{
+printf("Error");
+exit(0);
+
+}
+for (j = 1; j <= n; j++)
+{
+if (i != j)
+{
+ratio = a[j][i] / a[i][i];
+for (k = 1; k <= 2 * n; k++)
+{
+a[j][k] = a[j][k] - ratio * a[i][k];
+}
+}
+}
+}
+for (i = 1; i <= n; i++)
+{
+for (j = n + 1; j <= 2 * n; j++)
+{
+a[i][j] = a[i][j] / a[i][i];
+}
+}
+printf("\nInverse Matrix is:\n");
+for (i = 1; i <= n; i++)
+{
+for (j = n + 1; j <= 2 * n; j++)
+{
+printf("%0.3f\t", a[i][j]);
+}
+printf("\n");
+}
+return 0;
+}
